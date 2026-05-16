b12e5106904f1bd3815225966b36e7fc2cbe1f75
Николай <usenkko.nikolay@gmail.com>
2026-05-16 03:35:59 +0300

    Create ViewModels - RegisterViewModel and LoginViewModel

---
+++ SmartDiary.Web/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartDiary.Web.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Запомнить меня")]
+        public bool RememberMe { get; set; }
+    }
+}
